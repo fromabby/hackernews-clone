06fec70abb11dbbb1a6ef47431ef8d809ca00394
fromabby <60252891+fromabby@users.noreply.github.com>
2022-07-13 00:53:50 +0800

    added subscriptions

--- client/src/components/links/LinkList.js
+++ client/src/components/links/LinkList.js
@@ -1,9 +1,34 @@
 import Link from './Link'
 import { useQuery } from '@apollo/client'
 import { ALL_LINKS_QUERY } from '../../gql/queries'
+import { NEW_LINKS_SUBSCRIPTION, NEW_VOTES_SUBSCRIPTION } from '../../gql/subscriptions'
 
 const LinkList = () => {
-    const { loading, data, error } = useQuery(ALL_LINKS_QUERY)
+    const { loading, data, error, subscribeToMore } = useQuery(ALL_LINKS_QUERY)
+
+    subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev
+            const newLink = subscriptionData.data.newLink
+            const exists = prev.allLinks.links.find(
+                ({ id }) => id === newLink.id
+            )
+            if (exists) return prev
+
+            return Object.assign({}, prev, {
+                allLinks: {
+                    links: [newLink, ...prev.allLinks.links],
+                    count: prev.allLinks.links.length + 1,
+                    __typename: prev.allLinks.__typename
+                }
+            })
+        }
+    })
+
+    subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION
+    })
 
     return (
         <div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,25 @@
+import './styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import reportWebVitals from './utils/reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache, split } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { AUTH_TOKEN } from './constants'
-import './styles/index.css'
+
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN)
+        }
+    }
+})
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
@@ -23,7 +36,20 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
-const link = authLink.concat(httpLink)
+const isSubscription = ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return (kind === 'OperationDefinition' && operation === 'subscription')
+}
+
+/**
+ * split is used to “route” a request to a specific middleware link. It takes three arguments,
+ * the first one is a test function which returns a boolean.
+ * The remaining two arguments are again of type ApolloLink.
+ * If test returns true, the request will be forwarded to the link passed as the second argument.
+ * If false, to the third one.
+ */
+const link = split(isSubscription, wsLink, authLink.concat(httpLink))
+
 const cache = new InMemoryCache()
 
 const client = new ApolloClient({ link, cache })
